c140d38283bc841918266806b7824cdfb6a7d4d8
rmerriman <rmerriman@hortonworks.com>
2014-05-06 15:58:22 -0500

    new hbase class

--- solr/src/main/java/com/aig/hadoop/SOLRWriter.java
+++ solr/src/main/java/com/aig/hadoop/SOLRWriter.java
@@ -10,48 +10,48 @@ import java.io.IOException;
  * Created by mannd on 5/1/14.
  */
 public class SOLRWriter {
-    private CloudSolrServer solr;
+  private CloudSolrServer solr;
 
-    public void open() throws IOException {
-        solr = new CloudSolrServer("localhost:2181");
-        solr.setDefaultCollection("collection1");
+  public void open() throws IOException {
+    solr = new CloudSolrServer("localhost:2181");
+    solr.setDefaultCollection("collection1");
+  }
+
+  private SolrInputDocument convertToSOLR(Note note) {
+    final SolrInputDocument inputDoc = new SolrInputDocument();
+    inputDoc.addField("id", note.getID());
+    inputDoc.addField("type", note.getType());
+    inputDoc.addField("note", note.getText());
+
+    inputDoc.setDocumentBoost(1.0f);
+    return inputDoc;
+  }
+
+  public void write(Note note) throws IOException {
+    final SolrInputDocument inputDoc = convertToSOLR(note);
+    try {
+      solr.add(inputDoc);
+    } catch (SolrServerException e) {
+      throw makeIOException(e);
     }
+  }
 
-    SolrInputDocument convertToSOLR(Note note) {
-        final SolrInputDocument inputDoc = new SolrInputDocument();
-        inputDoc.addField("id", note.getID());
-        inputDoc.addField("type", note.getType());
-        inputDoc.addField("note", note.getText());
-
-        inputDoc.setDocumentBoost(1.0f);
-        return inputDoc;
+  public void flush() throws IOException {
+    try {
+      solr.commit(false, false);
+    } catch (final SolrServerException e) {
+      throw makeIOException(e);
     }
+  }
 
-    public void write(Note note) throws IOException {
-        final SolrInputDocument inputDoc = convertToSOLR(note);
-        try {
-            solr.add(inputDoc);
-        } catch (SolrServerException e) {
-            throw makeIOException(e);
-        }
-    }
+  public void close() throws IOException {
+    this.flush();
+    solr.shutdown();
+  }
 
-    public void flush() throws IOException {
-        try {
-            solr.commit(false, false);
-        } catch (final SolrServerException e) {
-            throw makeIOException(e);
-        }
-    }
-
-    public void close() throws IOException {
-        this.flush();
-        solr.shutdown();
-    }
-
-    private static IOException makeIOException(SolrServerException e) {
-        final IOException ioe = new IOException();
-        ioe.initCause(e);
-        return ioe;
-    }
+  private static IOException makeIOException(SolrServerException e) {
+    final IOException ioe = new IOException();
+    ioe.initCause(e);
+    return ioe;
+  }
 }
